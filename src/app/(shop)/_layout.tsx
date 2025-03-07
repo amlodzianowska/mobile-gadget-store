63ee81d85d4454c208f4861f0bdfbc98a1be9424
Anna Mlodzianowska <90103679+amlodzianowska@users.noreply.github.com>
2025-03-06 23:23:58 -0600

    add styling to shop layout tabs

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -4,7 +4,17 @@ import { SafeAreaView } from "react-native-safe-area-context"
 const TabsLayout = () => {
     return (
         <SafeAreaView>
-            <Tabs>
+            <Tabs screenOptions={{
+                tabBarActiveTintColor: "#1BC464",
+                tabBarInactiveTintColor: "gray",
+                tabBarLabelStyle: { fontSize: 16 },
+                tabBarStyle: {
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,
+                    paddingTop: 10
+                },
+                headerShown: false
+            }}>
                 <Tabs.Screen name='index' options={{headerShown: false}}/>
                 <Tabs.Screen name='orders' options={{}}/>
             </Tabs>
