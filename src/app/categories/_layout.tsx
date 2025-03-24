a2b1615390c9b0a1159c3403d7966589544540dc
Anna Mlodzianowska <90103679+amlodzianowska@users.noreply.github.com>
2025-03-24 00:06:14 -0500

    category page layout

--- src/app/categories/_layout.tsx
+++ src/app/categories/_layout.tsx
@@ -1,18 +1,21 @@
 import { Stack } from "expo-router";
 import { TouchableOpacity } from "react-native";
-import { Ionicons } from '@expo/vector-icons'
+import { Ionicons } from "@expo/vector-icons";
 
-export default function CategorysLayout() {
-    return <Stack>
-        <Stack.Screen 
-            name='[slug]' 
-            options={({navigation}) => ({
-                headerShown: false,
-                headerLeft:() => (
-                    <TouchableOpacity onPress={() => navigation.goBack()}>
-                        <Ionicons name='arrow-back' size={24} color='black'/>
-                    </TouchableOpacity>
-                )})} 
-        />
+export default function CategoryLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="[slug]"
+        options={({ navigation }) => ({
+          headerShown: true,
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+        })}
+      />
     </Stack>
+  );
 }
